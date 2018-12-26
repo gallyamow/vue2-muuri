e00cdbba9bb3e53af58d66e9ac95f52b794ddf10
Ramil Gallyamov <gallyamov.ramil@tatar.ru>
2018-12-26 17:27:31 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import Grid from './components/Grid'
+import Item from './components/Item'
+
+/**
+ * @const {string} muuri version.
+ */
+const VERSION = 'C_PKG_VERSION'
+
+/**
+ * Registers all VueLayers components.
+ * @param {Vue|VueConstructor} Vue
+ * @param {Vue2MuuriOptions} [options]
+ */
+function plugin (Vue, options = {}) {
+  if (plugin.installed) {
+    return
+  }
+  plugin.installed = true
+
+  Vue.use(Grid, options)
+  Vue.use(Item, options)
+}
+
+export default plugin
+
+export {
+  VERSION,
+  plugin as install,
+  Grid,
+  Item
+}
+
+/**
+ * @typedef {Object} Vue2MuuriOptions
+ * @property {string} [prop1] Prop1 description
+ */
---
+++ test/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <Grid/>
+</template>
+
+<script>
+  import Grid from '../src/components/Grid.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      Grid
+    }
+  }
+</script>
+
+<style>
+</style>
